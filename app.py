be1f90f50673ce783377533294a5f895404ecc9a
anandkunchala1965 <anand.kunchala@gmail.com>
2026-03-27 11:07:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 
 # ===== CONFIG =====
 TOKEN = os.getenv("KOTAK_TOKEN")
-BASE_URL = "https://api.kotaksecurities.com"  # will adjust if needed
-
+BASE_URL = "https://mis.kotaksecurities.com"
 LOT_SIZE = 1  # 1 lot only
 
 # ===== PLACE ORDER FUNCTION =====
@@ -23,21 +22,20 @@ def place_order(symbol, side, price):
     }
 
     order_data = {
-        "am": "NO",
-        "dq": "0",
-        "es": "nse_fo",              # OPTIONS
-        "mp": "0",
-        "pc": "MIS",
-        "pf": "N",
-        "pr": "0",
-        "pt": "L",                  # LIMIT ORDER (IMPORTANT)
-        "qt": str(LOT_SIZE),
-        "rt": "DAY",
-        "tp": "0",
-        "ts": symbol,
-        "tt": "B" if side == "BUY" else "S",
-        "price": str(price)
-    }
+    "am": "NO",
+    "dq": "0",
+    "es": "nse_fo",
+    "mp": "0",
+    "pc": "MIS",
+    "pf": "N",
+    "pr": str(price),     
+    "pt": "LMT",          
+    "qt": str(LOT_SIZE),
+    "rt": "DAY",
+    "tp": "0",
+    "ts": symbol,
+    "tt": "B" if side == "BUY" else "S"
+}
 
     payload = f"jData={json.dumps(order_data)}"
 
